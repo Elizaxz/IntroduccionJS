75ff32f62cbd19fa91dd2ca5fc5a82b9d5cfed18
Elizaxz <elizabethh.folz@gmail.com>
2025-09-17 00:15:45 -0600

    primer inivio de jvscrip

---
+++ js/01.js
@@ -0,0 +1,24 @@
+// variables
+var poducto = 'audifonos gaimer'; //iniciar variable y asignarle valor 
+
+var disponible; // iniciar la variable pero sin valor 
+
+producto = true; // reasignando el valor de la variable
+
+disponible = true; 
+
+var producto1 = 'computadora'
+    disponible = true,
+    categoria = 'computadoras';
+
+// variables indisponibles
+var disponible_;
+
+// estilos para las variables
+var nombre_producto = 'Monitor HD'; // UNDERSCORE
+var nombreProducto = 'MonitorHD';  //  ESTE SE CONOSE COMO CAMELCASE
+var NombreProducto = 'Monitor HD'; // ESTO ES PARA CUANDO SE CREA UNA CLASE pascalcase
+var nombreproducto = 'monitor HD';
+
+
+console.log(producto1);
