55ba89d9d01019cd33c96108de8b6655f3642da3
Elizaxz <elizabethh.folz@gmail.com>
2025-09-25 22:19:59 -0600

    conociendo elementos de java

---
+++ js/13.js
@@ -0,0 +1,23 @@
+//Objetos  ¿como unir objetos?
+
+
+
+const producto = {
+    nombreProducto : "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+const medidas = {
+    peso: '1kg',
+    medida: '1m'
+}
+
+//para unir ambos objetos  para no ,modificar los datos originales
+
+
+const nuevoProducto = {...producto,...medidas };
+
+
+console.log(producto);
+console.log(nuevoProducto);
